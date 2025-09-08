b5006d204c7ef3c54e2dfaed7a2e1d81685e64a1
Klecianny Melo <klecianny.melo@quorum.us>
2025-09-08 17:25:50 -0300

    :sparkles: Fibonacci Sequence 1

---
+++ chapter-1/1. Fibonacci sequence.py
@@ -0,0 +1,20 @@
+# Fibonacci Sequence
+
+# The sum of two numbers is the next number in the sequence
+# Except the first two numbers. They are 0 and 1
+# Example: 0, 1, 1, 2, 3, 5, 8, 13, 21...
+
+# Formula
+# fib(n) = fib(n - 1) + fib(n - 2)
+
+def fib1(n: int) -> int:
+    return fib1(n - 1) + fib1(n - 2)
+
+if __name__ == "__main__":
+    print(fib1(5))
+
+# ERROR: RecursionError: maximum recursion depth exceeded
+
+# Example of infinity recursion
+# def f():
+#     f()
