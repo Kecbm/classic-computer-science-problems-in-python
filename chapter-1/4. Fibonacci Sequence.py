efc34a7caef68c22c93d70cb7709798df7649848
Klecianny Melo <klecianny.melo@quorum.us>
2025-09-10 19:29:01 -0300

    :sparkles: fibonacci sequence 4

--- chapter-1/4. Fibonacci Sequence.py
+++ chapter-1/4. Fibonacci Sequence.py
@@ -1,3 +1,17 @@
 # Fibonacci Sequence
+         
+# Automatic memoization with decorator: @functtools.lru_cache()
 
-# PAGE: 27
+from functools import lru_cache
+
+# maxsize defines the maximum number of entries in the cache
+# maxsize=None means that the cache will grow indefinitely
+@lru_cache(maxsize=None)
+def fib4(n: int) -> int: # same definition of fib2()
+    if n < 2:
+        return n # base case
+    
+    return fib4(n - 2) + fib4(n - 1) # recursive case
+
+if __name__ == "__main__":
+    print(fib4(50))
