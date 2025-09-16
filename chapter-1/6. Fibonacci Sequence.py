a760f388457472e9ebe8cdddcd731db5adbc914b
Klecianny Melo <klecianny.melo@quorum.us>
2025-09-16 12:32:24 -0300

    :sparkles: fib6: genarating the numbers of the fibonacci sequence

--- chapter-1/6. Fibonacci Sequence.py
+++ chapter-1/6. Fibonacci Sequence.py
@@ -1,3 +1,73 @@
 # Fibonacci Sequence
 
-# PAGE 29
+from typing import Generator
+
+def fib6(n: int) -> Generator[int, None, None]:
+    yield 0 # special case
+
+    if n > 0: yield 1 # special case
+
+    last: int = 0 # initially set to fib(0)
+    next: int = 1 # initially set to fib(1)
+
+    for _ in range(1, n):
+        last, next = next, last + next
+        yield next # main generation step
+
+if __name__ == "__main__":
+    for i in fib6(50):
+        print(i)
+
+"""
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+233
+377
+610
+987
+1597
+2584
+4181
+6765
+10946
+17711
+28657
+46368
+75025
+121393
+196418
+317811
+514229
+832040
+1346269
+2178309
+3524578
+5702887
+9227465
+14930352
+24157817
+39088169
+63245986
+102334155
+165580141
+267914296
+433494437
+701408733
+1134903170
+1836311903
+2971215073
+4807526976
+7778742049
+12586269025
+"""
